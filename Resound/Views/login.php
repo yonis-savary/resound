a2ba28a4b578f717f8138bfff977df281e400400
yonis-savary <yonissavary@outlook.fr>
2024-03-12 10:37:19 +0100

    Favicon on login page

--- Resound/Views/login.php
+++ Resound/Views/login.php
@@ -16,6 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= asset("resound.png") ?>" type="image/x-icon">
     <?= style("assets-kit/style.css") ?>
     <title>Re-Sound - Login</title>
 </head>
