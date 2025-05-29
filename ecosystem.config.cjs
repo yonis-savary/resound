ea94b15d092d7c8cb5c947993c5e93f16d3de522
yonis-savary <yonissavary@outlook.fr>
2025-05-30 00:49:33 +0200

    Added pm2 config file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	apps: [{
+	      	name: 'resound',
+      		script: './.output/server/index.mjs',
+		env: {
+		        PORT: 8081,
+	        	NODE_ENV: 'production'
+		}
+	}]
+}
