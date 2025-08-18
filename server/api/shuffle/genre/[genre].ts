705ae65e8d35cad5e1cb9f54af631661498492cb
yonis-savary <yonis.savary@koesio.com>
2025-08-18 10:54:08 +0200

    fix: bad type conversion for /shuffle/genre

--- server/api/shuffle/genre/[genre].ts
+++ server/api/shuffle/genre/[genre].ts
@@ -1,5 +1,4 @@
 import { Op, Sequelize } from "sequelize";
-import { albumBaseIncludes } from "~/server/helpers/includes";
 import models from "~/server/db/models";
 
 export default defineEventHandler(async event => {
@@ -11,28 +10,54 @@ export default defineEventHandler(async event => {
         ? { id: { [Op.in]: Sequelize.literal(`(SELECT track FROM user_like WHERE "user" = ${user})`) } }
         : {}
 
+    // Wanted to do this in one query, the {where: {name: genre}} in nested includes won't work
+    // So this solution was chosen instead
+
+    const albums = await models.Album.findAll({
+        attributes: ["id"],
+        include: [
+            {
+                model: models.AlbumGenre,
+                as: "album_genres",
+                required: true,
+                include: [
+                    {
+                        model: models.Genre,
+                        as: "genre_genre",
+                        required: true,
+                        where: { name: genre }
+                    }
+                ]
+            }
+        ]
+    });
+
+    const albumIds = albums.map(a => a.id);
+
     return await models.Track.findAll({
-        where: { ...options },
+        where: {
+            ...options,
+            album: albumIds
+        },
         order: [Sequelize.fn('RANDOM')],
         include: [
             {
                 model: models.Album,
                 as: 'album_album',
                 include: [{
-                    model: models.AlbumGenre,
-                    as: 'album_genres',
-                    where: { genre }
-                }, ...albumBaseIncludes]
+                    model: models.AlbumArtist,
+                    as: 'album_artists'
+                }]
             },
             {
                 model: models.TrackArtist,
                 as: 'track_artists',
                 include: [{
                     model: models.Artist,
-                    as: 'artist_artist'
+                    as: 'artist_artist',
                 }]
             },
         ],
         limit: 100
-    })
+    });
 })
